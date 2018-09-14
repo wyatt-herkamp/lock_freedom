97834adf42c33f28505abe002aedd0e4ed432268
Bruno Zimmermann <brunoczim@gmail.com>
2018-09-14 00:16:35 -0300

    switched to a better profiler

--- make-profile.sh
+++ make-profile.sh
@@ -10,14 +10,5 @@ pushd profiling > /dev/null || exit 1
 RUSTFLAGS=-g cargo build --release --bin "$1" || exit 1
 popd > /dev/null || exit 1
 
-export PROFILING=1
-
-FLAGS=
-
-if [ -n "$PROF_FREQ" ]
-then
-    FLAGS=--freq="$PROF_FREQ"
-fi
-
-perf record --call-graph=dwarf $FLAGS profiling/target/release/"$1" \
+vallgrind --tool=callgrind --demangle=yes ./profiling/target/release/"$1" \
     || exit 1
