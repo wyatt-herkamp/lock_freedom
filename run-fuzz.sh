f460765f5a89f010b22a4314fc9527d01495b766
Bruno Zimmermann <brunoczim@gmail.com>
2018-09-17 01:15:57 -0300

    increased fuzz test's input length

--- run-fuzz.sh
+++ run-fuzz.sh
@@ -4,4 +4,4 @@
 export LSAN_OPTIONS='fast_unwind_on_malloc=0'
 export ASAN_OPTIONS='fast_unwind_on_malloc=0'
 
-cargo fuzz run $@ -- -trace_malloc=[12]
+cargo fuzz run $@ -- -trace_malloc=[12] -max_len=1048576
