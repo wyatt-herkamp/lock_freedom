0ecb7f2d383418ac1d5b82f75efd96ca4300fd20
Bruno Zimmermann <brunoczim@gmail.com>
2018-10-30 00:12:46 -0300

    Revert "optional owned access pointer"
    This reverts commit a22f6a98e1695b6d570d3995b7cd838e75a2eced.

--- src/atomic_smart_ptr/owned.rs
+++ src/atomic_smart_ptr/owned.rs
@@ -1,6 +1,4 @@
 use super::{
-    owned_alloc_to_raw,
-    raw_to_owned_alloc,
     CasError,
     Ordering::{self, *},
 };
@@ -8,10 +6,7 @@ use owned_alloc::OwnedAlloc;
 use std::{fmt, ptr::NonNull, sync::atomic::AtomicPtr};
 
 pub type OwnedCasError<'target, T> =
-    CasError<OwnedAlloc<T>, OwnedPreview<'target, T>>;
-
-pub type OptionOwnedCasError<'target, T> =
-    CasError<Option<OwnedAlloc<T>>, OptionOwnedPreview<'target, T>>;
+    CasError<OwnedAlloc<T>, OwnedAccessPreview<'target, T>>;
 
 pub struct OwnedAccessPtr<T> {
     atomic: AtomicPtr<T>,
@@ -19,11 +14,11 @@ pub struct OwnedAccessPtr<T> {
 
 impl<T> OwnedAccessPtr<T> {
     pub fn new(alloc: OwnedAlloc<T>) -> Self {
-        Self { atomic: AtomicPtr::new(alloc.into_raw().as_ptr()) }
+        Self::from(alloc)
     }
 
-    pub fn load(&self, ordering: Ordering) -> OwnedPreview<T> {
-        OwnedPreview {
+    pub fn load(&self, ordering: Ordering) -> OwnedAccessPreview<T> {
+        OwnedAccessPreview {
             preview: unsafe {
                 NonNull::new_unchecked(self.atomic.load(ordering))
             },
@@ -58,8 +53,8 @@ impl<T> From<T> for OwnedAccessPtr<T> {
 }
 
 impl<T> From<OwnedAlloc<T>> for OwnedAccessPtr<T> {
-    fn from(alloc: OwnedAlloc<T>) -> Self {
-        Self::new(alloc)
+    fn from(owned: OwnedAlloc<T>) -> Self {
+        Self { atomic: AtomicPtr::new(owned.into_raw().as_ptr()) }
     }
 }
 
@@ -79,12 +74,12 @@ impl<T> fmt::Debug for OwnedAccessPtr<T> {
     }
 }
 
-pub struct OwnedPreview<'target, T> {
+pub struct OwnedAccessPreview<'target, T> {
     preview: NonNull<T>,
     target: &'target OwnedAccessPtr<T>,
 }
 
-impl<'target, T> OwnedPreview<'target, T> {
+impl<'target, T> OwnedAccessPreview<'target, T> {
     pub fn pointer(&self) -> NonNull<T> {
         self.preview
     }
@@ -172,137 +167,11 @@ impl<'target, T> OwnedPreview<'target, T> {
     }
 }
 
-impl<'target, T> fmt::Debug for OwnedPreview<'target, T> {
+impl<'target, T> fmt::Debug for OwnedAccessPreview<'target, T> {
     fn fmt(&self, fmtr: &mut fmt::Formatter) -> fmt::Result {
         write!(
             fmtr,
-            "OwnedPreview {} preview: {:?}, target {:?} {}",
-            '{', self.preview, self.target, '}'
-        )
-    }
-}
-
-pub struct OptionOwnedAccessPtr<T> {
-    atomic: AtomicPtr<T>,
-}
-
-impl<T> OptionOwnedAccessPtr<T> {
-    pub fn new(alloc: Option<OwnedAlloc<T>>) -> Self {
-        Self { atomic: AtomicPtr::new(owned_alloc_to_raw(alloc)) }
-    }
-
-    pub fn load(&self, ordering: Ordering) -> OptionOwnedPreview<T> {
-        OptionOwnedPreview { preview: self.atomic.load(ordering), target: self }
-    }
-
-    pub fn swap(
-        &self,
-        alloc: Option<OwnedAlloc<T>>,
-        ordering: Ordering,
-    ) -> Option<OwnedAlloc<T>> {
-        let ptr = self.atomic.swap(owned_alloc_to_raw(alloc), ordering);
-        unsafe { raw_to_owned_alloc(ptr) }
-    }
-
-    pub fn store(&self, alloc: Option<OwnedAlloc<T>>, ordering: Ordering) {
-        match ordering {
-            Acquire => panic!("No such thing as Acquire for a store"),
-            AcqRel => panic!("No such thing as AcqRel for a store"),
-            _ => (),
-        }
-
-        self.swap(alloc, ordering);
-    }
-}
-
-impl<T> From<T> for OptionOwnedAccessPtr<T> {
-    fn from(val: T) -> Self {
-        Self::from(OwnedAlloc::new(val))
-    }
-}
-
-impl<T> From<Option<T>> for OptionOwnedAccessPtr<T> {
-    fn from(opt: Option<T>) -> Self {
-        match opt {
-            Some(val) => Self::from(val),
-            None => Self::new(None),
-        }
-    }
-}
-
-impl<T> From<OwnedAlloc<T>> for OptionOwnedAccessPtr<T> {
-    fn from(alloc: OwnedAlloc<T>) -> Self {
-        Self::from(Some(alloc))
-    }
-}
-
-impl<T> From<Option<OwnedAlloc<T>>> for OptionOwnedAccessPtr<T> {
-    fn from(alloc: Option<OwnedAlloc<T>>) -> Self {
-        Self::new(alloc)
-    }
-}
-
-impl<T> Drop for OptionOwnedAccessPtr<T> {
-    fn drop(&mut self) {
-        unsafe {
-            raw_to_owned_alloc(self.atomic.load(Relaxed));
-        }
-    }
-}
-
-impl<T> fmt::Debug for OptionOwnedAccessPtr<T> {
-    fn fmt(&self, fmtr: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            fmtr,
-            "OptionOwnedAccessPtr {} atomic: {:?} {}",
-            '{', self.atomic, '}'
-        )
-    }
-}
-
-pub struct OptionOwnedPreview<'target, T> {
-    preview: *mut T,
-    target: &'target OptionOwnedAccessPtr<T>,
-}
-
-impl<'target, T> OptionOwnedPreview<'target, T> {
-    pub fn pointer(&self) -> *mut T {
-        self.preview
-    }
-
-    pub fn target(&self) -> &OptionOwnedAccessPtr<T> {
-        self.target
-    }
-
-    pub fn compare_and_swap(
-        self,
-        new_value: Option<OwnedAlloc<T>>,
-        ordering: Ordering,
-    ) -> Result<Option<OwnedAlloc<T>>, OptionOwnedCasError<'target, T>> {
-        let new_raw = owned_alloc_to_raw(new_value);
-        let res = self.target.atomic.compare_and_swap(
-            self.preview,
-            new_raw,
-            ordering,
-        );
-
-        if res == self.preview {
-            Ok(unsafe { raw_to_owned_alloc(self.preview) })
-        } else {
-            Err(CasError {
-                found: Self { preview: res, target: self.target },
-                expected: self,
-                desired: unsafe { raw_to_owned_alloc(new_raw) },
-            })
-        }
-    }
-}
-
-impl<'target, T> fmt::Debug for OptionOwnedPreview<'target, T> {
-    fn fmt(&self, fmtr: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            fmtr,
-            "OwnedPreview {} preview: {:?}, target {:?} {}",
+            "OwnedAccessPreview {} preview: {:?}, target {:?} {}",
             '{', self.preview, self.target, '}'
         )
     }
