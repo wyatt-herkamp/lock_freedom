3281dfe14c50678a0daa92bfe1e0ae810f471d4f
Bruno Zimmermann <brunoczim@gmail.com>
2018-10-31 18:08:35 -0300

    basic initialization of map

--- src/map/mod.rs
+++ src/map/mod.rs
@@ -16,11 +16,52 @@ use self::{
 use atomic::AtomicBoxIncin;
 use incin::Incinerator;
 use owned_alloc::OwnedAlloc;
-use std::sync::Arc;
+use std::{
+    hash::{BuildHasher, Hash, Hasher},
+    sync::Arc,
+};
 
 pub struct Map<K, V, H = RandomState> {
     top: OwnedAlloc<Table<K, V>>,
     incin: Arc<Incinerator<Garbage<K, V>>>,
     box_incin: AtomicBoxIncin<Elem<K, V>>,
     builder: H,
+}
+
+impl<K, V> Map<K, V> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl<K, V, H> Map<K, V, H>
+where
+    H: BuildHasher,
+{
+    pub fn with_hasher(builder: H) -> Self {
+        Self {
+            top: Table::new_alloc(),
+            incin: Arc::new(Incinerator::new()),
+            box_incin: AtomicBoxIncin::new(),
+            builder,
+        }
+    }
+
+    fn hash_of<Q>(&self, key: &Q) -> u64
+    where
+        Q: ?Sized + Hash,
+    {
+        let mut hasher = self.builder.build_hasher();
+        key.hash(&mut hasher);
+        hasher.finish()
+    }
+}
+
+impl<K, V, H> Default for Map<K, V, H>
+where
+    H: BuildHasher + Default,
+{
+    fn default() -> Self {
+        Self::with_hasher(H::default())
+    }
 }
--- src/map/table.rs
+++ src/map/table.rs
@@ -1,4 +1,5 @@
-use std::{marker::PhantomData, sync::atomic::AtomicPtr};
+use owned_alloc::{OwnedAlloc, UninitAlloc};
+use std::{marker::PhantomData, ptr::null_mut, sync::atomic::AtomicPtr};
 
 const BITS: usize = 8;
 
@@ -7,7 +8,28 @@ pub struct Table<K, V> {
     nodes: [Node<K, V>; 1 << BITS],
 }
 
+impl<K, V> Table<K, V> {
+    pub fn new_alloc() -> OwnedAlloc<Self> {
+        unsafe {
+            UninitAlloc::<Self>::new().init_in_place(|val| val.init_in_place())
+        }
+    }
+
+    pub unsafe fn init_in_place(&mut self) {
+        for node in &mut self.nodes as &mut [_] {
+            (node as *mut Node<K, V>).write(Node::new())
+        }
+    }
+}
+
 struct Node<K, V> {
+    // First lower bit is 0 for leaf and 1 for branch
     atomic: AtomicPtr<()>,
     _marker: PhantomData<(K, V)>,
+}
+
+impl<K, V> Node<K, V> {
+    fn new() -> Self {
+        Self { atomic: AtomicPtr::new(null_mut()), _marker: PhantomData }
+    }
 }
