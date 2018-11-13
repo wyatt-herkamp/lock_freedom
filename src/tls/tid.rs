5b8956e75f8bf8aaf2635b8148abc473fe731cac
Bruno Zimmermann <brunoczim@gmail.com>
2018-11-12 22:10:30 -0200

    new wait-free id handling

--- src/tls/tid.rs
+++ src/tls/tid.rs
@@ -1,6 +1,8 @@
+use owned_alloc::OwnedAlloc;
 use std::{
     marker::PhantomData,
-    sync::atomic::{AtomicUsize, Ordering::*},
+    ptr::{null_mut, NonNull},
+    sync::atomic::{AtomicBool, AtomicPtr, AtomicUsize, Ordering::*},
 };
 
 /// A cached thread-id. Repeated calls to [`ThreadLocal`](super::ThreadLocal)'s
@@ -16,7 +18,7 @@ impl ThreadId {
     #[inline]
     pub fn current() -> Self {
         ID.with(|id| Self {
-            bits: id.bits,
+            bits: id.node.bits,
             _non_tsafe: PhantomData,
         })
     }
@@ -33,29 +35,50 @@ impl Default for ThreadId {
 }
 
 static COUNTER: AtomicUsize = AtomicUsize::new(0);
+static ID_LIST: AtomicPtr<Node> = AtomicPtr::new(null_mut());
 
 thread_local! {
     static ID: IdGuard = IdGuard::new();
 }
 
 struct IdGuard {
-    bits: usize,
+    node: &'static Node,
 }
 
 impl IdGuard {
     fn new() -> Self {
-        loop {
-            let bits = COUNTER.load(Acquire);
-            let new = bits.checked_add(1).expect("Too many threads");
-            if COUNTER.compare_and_swap(bits, new, Release) == bits {
-                break Self { bits };
+        let mut loaded = ID_LIST.load(Acquire);
+
+        while let Some(nnptr) = NonNull::new(loaded) {
+            let node = unsafe { &*nnptr.as_ptr() };
+            if node.free.swap(false, Acquire) {
+                return Self { node };
             }
+            loaded = node.next.load(Acquire);
         }
+
+        let free = AtomicBool::new(false);
+        let bits = COUNTER.fetch_add(1, Relaxed);
+        let next = AtomicPtr::new(null_mut());
+        let nnptr = OwnedAlloc::new(Node { free, bits, next }).into_raw();
+        let node = unsafe { &*nnptr.as_ptr() };
+
+        let in_place = ID_LIST.swap(nnptr.as_ptr(), Relaxed);
+        node.next.store(in_place, Release);
+
+        Self { node }
     }
 }
 
 impl Drop for IdGuard {
     fn drop(&mut self) {
-        COUNTER.compare_and_swap(self.bits + 1, self.bits, Release);
+        self.node.free.store(true, Relaxed);
     }
+}
+
+#[repr(align(/* at least */ 2))]
+struct Node {
+    free: AtomicBool,
+    bits: usize,
+    next: AtomicPtr<Node>,
 }
