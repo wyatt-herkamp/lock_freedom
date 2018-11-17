da50a80b6d60afcdff1feca1ddc92cb48b67f4e2
Bruno Zimmermann <brunoczim@gmail.com>
2018-11-17 19:51:46 -0200

    preparing patch version

--- src/tls/tid.rs
+++ src/tls/tid.rs
@@ -3,7 +3,7 @@ use std::{
     fmt,
     marker::PhantomData,
     ptr::null_mut,
-    sync::atomic::{AtomicBool, AtomicPtr, AtomicUsize, Ordering::*},
+    sync::atomic::{AtomicPtr, AtomicUsize, Ordering::*},
 };
 
 /// A cached thread-id. Repeated calls to [`ThreadLocal`](super::ThreadLocal)'s
@@ -18,7 +18,7 @@ impl ThreadId {
     /// Loads the ID for this thread.
     #[inline]
     pub fn current() -> Self {
-        ID.with(|id| Self { bits: id.node.bits, _non_tsafe: PhantomData })
+        ID.with(|id| Self { bits: id.bits, _non_tsafe: PhantomData })
     }
 
     pub(super) fn bits(self) -> usize {
@@ -40,11 +40,8 @@ impl fmt::Debug for ThreadId {
 
 static ID_COUNTER: AtomicUsize = AtomicUsize::new(1);
 
-static ID_LIST: Node = Node {
-    bits: 0,
-    free: AtomicBool::new(true),
-    next: AtomicPtr::new(null_mut()),
-};
+static ID_LIST: Node =
+    Node { free: AtomicUsize::new(0), next: AtomicPtr::new(null_mut()) };
 
 static ID_LIST_BACK: AtomicPtr<Node> =
     AtomicPtr::new(&ID_LIST as *const _ as *mut _);
@@ -53,34 +50,8 @@ thread_local! {
     static ID: IdGuard = IdGuard::new();
 }
 
-fn create_node() -> &'static Node {
-    let node = Node {
-        bits: 0,
-        free: AtomicBool::new(false),
-        next: AtomicPtr::new(null_mut()),
-    };
-
-    let mut alloc = OwnedAlloc::new(node);
-
-    // Let's change the counter after the allocation, so memory will be
-    // exhausted before the counter overflows. Allocation can be seen as
-    // side-effect, since it may perform a syscall. Therefore, it is not
-    // reordered.
-    alloc.bits = ID_COUNTER.fetch_add(1, Relaxed);
-
-    let nnptr = alloc.into_raw();
-
-    let prev = ID_LIST_BACK.swap(nnptr.as_ptr(), Release);
-
-    // Ok because the list back is never null. Also, nodes are either static
-    // variables or heap-allocations turned into static variables.
-    unsafe {
-        (*prev).next.store(nnptr.as_ptr(), Release);
-        &*nnptr.as_ptr()
-    }
-}
-
 struct IdGuard {
+    bits: usize,
     node: &'static Node,
 }
 
@@ -95,8 +66,9 @@ impl IdGuard {
 
         loop {
             // First we try to acquire the current node.
-            if node.free.swap(false, Relaxed) {
-                break Self { node };
+            let bits = node.free.swap(usize::max_value(), Relaxed);
+            if bits != usize::max_value() {
+                break Self { node, bits };
             }
 
             let next = node.next.load(Acquire);
@@ -104,7 +76,7 @@ impl IdGuard {
             // Then we check if we reached the limited we loaded previously.
             if next.is_null() || node as *const _ == back_then {
                 // If so, we create a new node.
-                break Self { node: create_node() };
+                break Self::create_node();
             }
 
             // Ok because nodes are either static variables or heap-allocations
@@ -112,17 +84,44 @@ impl IdGuard {
             node = unsafe { &*next };
         }
     }
+
+    fn create_node() -> Self {
+        let new = Node {
+            free: AtomicUsize::new(usize::max_value()),
+            next: AtomicPtr::new(null_mut()),
+        };
+
+        let alloc = OwnedAlloc::new(new);
+        let nnptr = alloc.into_raw();
+
+        let prev = ID_LIST_BACK.swap(nnptr.as_ptr(), AcqRel);
+
+        // Let's change the counter after the allocation and insertion, so
+        // memory will be exhausted before the counter overflows. Allocation
+        // can be seen as side-effect, since it may perform a syscall.
+        // Therefore, it is not reordered.
+        let bits = ID_COUNTER.fetch_add(1, Relaxed);
+
+        // Ok because the list back is never null. Also, nodes are either static
+        // variables or heap-allocations turned into static variables.
+        let node = unsafe {
+            (*prev).next.store(nnptr.as_ptr(), Release);
+            &*nnptr.as_ptr()
+        };
+
+        Self { node, bits }
+    }
 }
 
 impl Drop for IdGuard {
     fn drop(&mut self) {
-        self.node.free.store(true, Relaxed);
+        self.node.free.store(self.bits, Relaxed);
     }
 }
 
 #[repr(align(/* at least */ 2))]
 struct Node {
-    free: AtomicBool,
-    bits: usize,
+    // Set to usize::max_value() when not free.
+    free: AtomicUsize,
     next: AtomicPtr<Node>,
 }
